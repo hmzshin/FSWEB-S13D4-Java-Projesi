7f4486a802146faa49b2a71e243a38f5f5831154
hamza <hamasahin@outlook.com>
2024-01-11 14:10:37 +0300

    görev 1:Point Sınıfı tanımlar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point distance = new Point(6, 5);
+        Point distance1 = new Point(3, 1);
+        System.out.println("distance to origin:" + distance.distance());
+
+        System.out.println("distance given points:" + distance.distance(2, 2));
+
+        System.out.println("distance given Point Object:" + distance.distance(distance1));
     }
 }
---
+++ src/Point.java
@@ -0,0 +1,46 @@
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+
+    public double distance(Point p) {
+        return Math.sqrt(Math.pow((x - p.x), 2) + Math.pow((y - p.y), 2));
+    }
+
+    public double distance(int a, int b) {
+        return Math.sqrt(Math.pow((a - x), 2) + Math.pow((b - y), 2));
+    }
+
+
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
